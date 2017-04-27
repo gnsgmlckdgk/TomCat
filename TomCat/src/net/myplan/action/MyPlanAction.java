fb2a53666b93a578ad169744e5363d3f46aff8b1
kangsu <kshing44@gmail.com>
2017-04-27 17:44:05 +0900

    map db에서 정보받아오기 작업중

---
+++ TomCat/src/net/myplan/action/MyPlanAction.java
@@ -0,0 +1,32 @@
+package net.myplan.action;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.myplan.db.MyPlanBean;
+import net.myplan.db.MyPlanDAO;
+
+public class MyPlanAction implements Action{
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		
+		MyPlanDAO mpdo = new MyPlanDAO();
+		
+		List<MyPlanBean> MyPlan = mpdo.MyPlan();
+
+		
+		request.setAttribute("MyPlan", MyPlan);
+	
+		ActionForward forward=new ActionForward();
+		forward.setRedirect(false);
+		forward.setPath("./myplan/map_test.jsp");
+		return forward;
+
+	}
+
+	
+}
