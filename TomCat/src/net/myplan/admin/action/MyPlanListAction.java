e808ff751a52c278d3300d8098c10ddd0b179b2f
kangsu <kshing44@gmail.com>
2017-05-02 15:46:27 +0900

    myplan 입력, 리스트 출력 완료

--- TomCat/src/net/myplan/admin/action/MyPlanListAction.java
+++ TomCat/src/net/myplan/admin/action/MyPlanListAction.java
@@ -1,29 +1,26 @@
-package net.myplan.action;
+package net.myplan.admin.action;
 
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import net.myplan.db.MyPlanBean;
-import net.myplan.db.MyPlanDAO;
+import net.myplan.admin.db.MyPlanDAO;
 
-public class MyPlanAction implements Action{
 
+public class MyPlanListAction implements Action{
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
-		
-		MyPlanDAO mpdo = new MyPlanDAO();
-		
-	
-	
+		//디비객체 생성 
+		MyPlanDAO mpl=new MyPlanDAO();
+		//List myplanList = 메서드호출 getGoodsList()
+		List myplanList = mpl.getMyPlanList();
+		// myplanList 저장
+		request.setAttribute("myplanList", myplanList);
+		//이동 ./myplan/myplan.jsp
 		ActionForward forward=new ActionForward();
 		forward.setRedirect(false);
 		forward.setPath("./myplan/myplan.jsp");
 		return forward;
-
 	}
-
-	
 }
--- TomCat/src/net/myplan/admin/db/MyPlanBean.java
+++ TomCat/src/net/myplan/admin/db/MyPlanBean.java
@@ -1,7 +1,70 @@
 package net.myplan.admin.db;
 
 public class MyPlanBean {
-
+	private int travel_id;
+	private String type;
+	private String country_code;
+	private String city_code;
+	private String name;
+	private String latitude;
+	private String longitude;
+	private String info;
+	private String address;
+	
+	public int getTravel_id() {
+		return travel_id;
+	}
+	public void setTravel_id(int travel_id) {
+		this.travel_id = travel_id;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getCountry_code() {
+		return country_code;
+	}
+	public void setCountry_code(String country_code) {
+		this.country_code = country_code;
+	}
+	public String getCity_code() {
+		return city_code;
+	}
+	public void setCity_code(String city_code) {
+		this.city_code = city_code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+	public String getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+	public String getInfo() {
+		return info;
+	}
+	public void setInfo(String info) {
+		this.info = info;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
 	
 	
 }
