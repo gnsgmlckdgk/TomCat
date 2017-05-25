227a9425da949968864fd394cfed136c9d408e5f
gnsgmlckdgk <dohauzi@gmail.com>
2017-05-25 17:38:14 +0900

    DB작업 close 안되있는거 close 함

--- TomCat/src/net/plan/db/PlanDAO.java
+++ TomCat/src/net/plan/db/PlanDAO.java
@@ -817,6 +817,15 @@ public class PlanDAO {
 		} catch (Exception e) {
 			e.printStackTrace();
 
+		}finally {
+			try{
+				if(rs!=null) rs.close();
+				if(pstmt!=null) pstmt.close();
+				if(con!=null) con.close();
+				
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
 		}
 		return country_code;
 	}
